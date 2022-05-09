b2599784fb89b12c2f84979ba88757c2eb42bff9
Adrian Perkowski <adrian1205@poczta.onet.pl>
2022-05-09 17:12:01 +0200

    Correction of tasks

--- coderslab/bvariablesandoperators/Main02.java
+++ coderslab/bvariablesandoperators/Main02.java
@@ -2,11 +2,12 @@ package pl.coderslab.bvariablesandoperators;
 
 public class Main02 {
 
-	static void Ex2(int p1, String p2, int p3, String p4)	{
-		System.out.println(p4+ " "+p3+" "+p2+" "+p1);
-	}
-	public static void main (String [] args)	{
+	public static void main(String []args)	{
 
-		Ex2(1, " John" ,4, "Kowalski" );
+		byte nr1 = 94;
+		short nr2 = 257;
+		int result = nr1 + nr2;
+
+		System.out.println(result);
 	}
 }
